22953b2507b610170b540f503e5f237a57814ab8
Li Dong <dongli@lasg.iap.ac.cn>
2017-04-02 21:54:41 +0800

    Add some assertions in cartesian mesh test

--- src/test/test_cartesian_mesh.F90
+++ src/test/test_cartesian_mesh.F90
@@ -6,6 +6,8 @@ program test_cartesian_mesh
 
   implicit none
 
+  integer i, j
+
   call fut_test_case_init()
 
   call fut_test_case_create('cartesian_mesh')
@@ -14,6 +16,25 @@ program test_cartesian_mesh
   call cartesian_mesh_init(full_grid=[10, 10], half_grid=[10, 10], halo_width=[1, 1])
 
   call fut_assert_equal(lbound(mesh%x, 1), 0)
+  call fut_assert_equal(ubound(mesh%x, 1), 11)
+  call fut_assert_equal(lbound(mesh%x, 2), FULL)
+  call fut_assert_equal(ubound(mesh%x, 2), HALF)
+  call fut_assert_equal(lbound(mesh%y, 1), 0)
+  call fut_assert_equal(ubound(mesh%y, 1), 11)
+  call fut_assert_equal(lbound(mesh%y, 2), FULL)
+  call fut_assert_equal(ubound(mesh%y, 2), HALF)
+
+  call fut_assert_approximate(mesh%x(0,FULL), 0.9)
+  do i = 1, 10
+    call fut_assert_approximate(mesh%x(i,FULL), 0.1*(i-1))
+  end do
+  call fut_assert_approximate(mesh%x(11,FULL), 1.0)
+
+  call fut_assert_approximate(mesh%y(0,FULL), 0.9)
+  do j = 1, 10
+    call fut_assert_approximate(mesh%y(j,FULL), 0.1*(j-1))
+  end do
+  call fut_assert_approximate(mesh%y(11,FULL), 1.0)
 
   call fut_test_case_final()
 
